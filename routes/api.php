13a0156f4587cc5c861caee694eb9454b7670d14
lewys-miugo <lewiswambugu01@gmail.com>
2026-02-24 12:42:14 +0300

    Added auth api routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TaskController;
+use App\Http\Controllers\Api\AuthController;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('tasks', TaskController::class)
@@ -10,3 +11,8 @@ Route::middleware('auth:sanctum')->group(function () {
 
 Route::apiResource('tasks', TaskController::class)
     ->only(['index', 'show']);
+
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+
+    Route::middleware('auth:sanctum')->post('logout', [AuthController::class, 'logout']);
