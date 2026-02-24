8565b8ea573fd490fcb984450834330f83694935
lewys-miugo <lewiswambugu01@gmail.com>
2026-02-24 11:58:53 +0300

    Added store and update resource requests

---
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Enum;
+use App\Enums\TaskStatus;
+
+class UpdateTaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true; //security is in routes
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+            'title' => ['sometimes', 'string', 'max:255'],
+            'description' => ['nullable', 'string'],
+            'status' => ['sometimes', new Enum(TaskStatus::class)],
+            'due_date' => ['sometimes', 'date'],
+        ];
+    }
+}
